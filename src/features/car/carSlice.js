a846c9172b1c73cf9849dd18fc10835f83feb72f
Samuel Martinez <samuel.martinez.morales@gmail.com>
2022-10-18 13:17:03 -0500

    State creation with redux

--- src/features/car/carSlice.js
+++ src/features/car/carSlice.js
@@ -1,10 +1,10 @@
-import { createSlice, createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     cars: [ "Model S", "Model 3", "Model X", "Model Y" ]
 }
 
-const createSlice = createSlice({
+const carSlice = createSlice({
     name: "car",
     initialState,
     reducers: {}
