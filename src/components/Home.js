b8b52bf180ae3ced2431598c611570b3986fa154
Samuel Martinez <samuel.martinez.morales@gmail.com>
2022-10-17 22:29:03 -0500

    finish sections for tesla

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,9 +6,9 @@ function Home() {
   return (
     <Container>
         <Section 
-            title="Model S"
+            title="Model 3"
             description="Order Online for Touchless Delivery"
-            backgroundImg="model-s.jpg"
+            backgroundImg="model-3.jpg"
             leftBtnText="Custom Order"
             rightBtnText="Existing inventory"
         />
@@ -20,9 +20,9 @@ function Home() {
             rightBtnText="Existing inventory"
         />
         <Section 
-            title="Model 3"
+            title="Model S"
             description="Order Online for Touchless Delivery"
-            backgroundImg="model-3.jpg"
+            backgroundImg="model-s.jpg"
             leftBtnText="Custom Order"
             rightBtnText="Existing inventory"
         />
@@ -32,6 +32,27 @@ function Home() {
             backgroundImg="model-x.jpg"
             leftBtnText="Custom Order"
             rightBtnText="Existing inventory"
+        />
+        <Section 
+            title="Lowest Cost Solar Panels in America"
+            description="Money-back guarantee"
+            backgroundImg="solar-panel.jpg"
+            leftBtnText="Order now"
+            rightBtnText="Learn More"
+        />
+        <Section 
+            title="Solar for New Roofs"
+            description="Solar Roof Costs Less Than a New Roof Plus Solar Panel"
+            backgroundImg="solar-roof.jpg"
+            leftBtnText="Order now"
+            rightBtnText="Learn More"
+        />
+        <Section 
+            title="Accesories"
+            description=""
+            backgroundImg="accessories.jpg"
+            leftBtnText="Shop now"
+            // rightBtnText="Shop now"
         />
     </Container>
     
